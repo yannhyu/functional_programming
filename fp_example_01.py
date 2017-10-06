d8df1833fdd774f061e2270d18bff28aa5c390cc
Yann Yu <yann.yu@medlytix.com>
2017-10-06 11:30:10 -0400

    add lambda wrapper funciton

--- fp_example_01.py
+++ fp_example_01.py
@@ -1,3 +1,29 @@
 l_factorial = lambda n: 1 if n == 0 else n*l_factorial(n-1)
 
 assert(l_factorial(3) == 6)
+
+# Timing implementations
+
+# (1) The procedural way going line by line
+import time
+
+t0 = time.time()
+l_factorial(900)
+t1 = time.time()
+print('Took: %.5f s' % (t1-t0))
+
+# (2) The functional way with a wrapper function
+def timer(fnc, arg):
+    t0 = time.time()
+    fnc(arg)
+    t1 = time.time()
+    return t1-t0
+
+print('Took: %.5f s' % timer(l_factorial, 888))
+
+# (3) Fully functional way with lambda wrapper function
+l_timestamp = lambda fnc, arg: (time.time(), fnc(arg), time.time())
+l_diff = lambda t0, retval, t1: t1-t0
+l_timer = lambda fnc, arg: l_diff(*l_timestamp(fnc, arg))
+
+print('Took: %.5f s' % l_timer(l_factorial, 777))
