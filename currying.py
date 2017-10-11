2875ef6f7454ae722a9cecc963d9d3022d81cd84
Yann Yu <yann.yu@medlytix.com>
2017-10-11 11:55:17 -0400

    add curry

---
+++ currying.py
@@ -0,0 +1,22 @@
+# One argument per function
+# add(1,2,3) ---> add(1)(2)(3)
+from functools import partial
+
+# (1) Simple example
+def add(a, b, c):
+    return a + b + c
+
+print(add(10, 100, 1000))
+add_10 = partial(add, 10)
+add_10_100 = partial(add_10, 100)
+print(add_10_100(1000))
+
+# (2) currying
+from inspect import signature
+
+def curry(func):
+    def inner(arg):
+        # Exactly one argument, no need to curry
+        if len(signature(func).parameters) == 1:
+            return func(arg)    # no need for curry
+        return curry(partial(func, arg))
