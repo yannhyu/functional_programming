e243940a4848f396d4a8648c7453ab5fc541d84c
Yann Yu <yann.yu@medlytix.com>
2017-10-11 14:01:58 -0400

    add monad

---
+++ monads.py
@@ -0,0 +1,12 @@
+# Let variables decide how they are treated
+# (1) nan monad: not a number
+# (2) maybe monad: just or nothing
+# (3) list monad: map is an example, 
+#     a list ---> a map applied to it ---> new list
+
+def camelcase(s):
+    return ''.join([w.capitalize() for w in s.split('_')])
+
+print(camelcase('some_python_function_name'))
+
+    
