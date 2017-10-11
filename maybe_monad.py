7a496600e8c5f135000f31e8be23bab17cd6ca70
Yann Yu <yann.yu@medlytix.com>
2017-10-11 14:08:14 -0400

    add maybe monad

---
+++ maybe_monad.py
@@ -0,0 +1,26 @@
+from monads import camelcase
+
+class Just:
+    def __init__(self, value):
+        self._value = value
+
+    def bind(self, func):
+        try:
+            return Just(func(self._value))
+        except:
+            return Nothing()
+
+    def __repr__(self):
+        return self._value
+
+
+class Nothing:
+    def bind(self, func):
+        return Nothing()
+
+    def __repr__(self):
+        return "Nothing"
+
+
+if __name__ == '__main__':
+    print(Just('some_python_function_name_maybe').bind(camelcase))        
